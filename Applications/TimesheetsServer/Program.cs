5b0face320d0bc8e1e15107c0883b0dedb9cfc3f
sriram-trc <sriram_trc@yahoo.com>
2020-07-30 08:59:16 +0530

    service discovery

--- Applications/TimesheetsServer/Program.cs
+++ Applications/TimesheetsServer/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Steeltoe.Extensions.Configuration.CloudFoundry;
 using Steeltoe.Extensions.Logging;
+using Steeltoe.Discovery.Client;
+using Steeltoe.Common.Hosting;
 
 namespace TimesheetsServer
 {
@@ -15,6 +17,7 @@ namespace TimesheetsServer
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseCloudHosting()
                 .AddCloudFoundry()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
